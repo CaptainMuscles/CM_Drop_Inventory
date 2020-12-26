96dddab36c8223474016275a199d97c7a3538f5b
NathanHarbour <nathanharbour@gmail.com>
2020-12-26 17:54:08 -0600

    Initial Commit

---
+++ Source/CM_Drop_Inventory/DropInventoryMod.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace CM_Drop_Inventory
+{
+    public class DropInventoryMod : Mod
+    {
+        private static DropInventoryMod _instance;
+        public static DropInventoryMod Instance => _instance;
+
+        public DropInventoryMod(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("CM_Drop_Inventory");
+            harmony.PatchAll();
+
+            _instance = this;
+        }
+    }
+}
